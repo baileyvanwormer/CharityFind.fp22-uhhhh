40848e36ff4c70fcf9d4d3a2240ea040571de536
Bailey Van Wormer <bvan-wormer@unc.edu>
2022-04-02 12:22:54 -0400

    Initial Commit

---
+++ CharityFind/CharityFindApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharityFindApp.swift
+//  CharityFind
+//
+//  Created by Bailey Van Wormer on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct CharityFindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CharityFind/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CharityFind
+//
+//  Created by Bailey Van Wormer on 4/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
