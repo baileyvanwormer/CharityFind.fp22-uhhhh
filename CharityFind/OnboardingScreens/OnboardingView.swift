36ff431519de9ac50809d3d081f4f8d768fefb78
Bailey Van Wormer <bvan-wormer@unc.edu>
2022-04-05 13:37:12 -0400

    Initial commit; Nothing has been done other than file creation and naming

---
+++ CharityFind/OnboardingScreens/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  CharityFind
+//
+//  Created by Bailey Van Wormer on 4/2/22.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
